03d9656c0a09f59efa10c478bb2569758f01e5fd
Caleb Denio <cjdenio44@gmail.com>
2024-05-05 17:30:59 -0400

    Update upload.ts

--- api/v2/upload.ts
+++ api/v2/upload.ts
@@ -46,7 +46,7 @@ const upload = async (url: string, authorization: string | null) => {
 export default async (req: Request) => {
   if (!ensurePost(req)) return null;
 
-  const body =await req.arrayBuffer();
+  const body = new TextDecoder().decode(await req.arrayBuffer());
   const uploadedFileUrl = await upload(body, req.headers.get("Authorization"));
 
   return new Response(JSON.stringify(uploadedFileUrl));
