5efab013d427f51ae41be332488cc1dd210eb76c
24c02 <163450896+24c02@users.noreply.github.com>
2026-01-27 19:36:12 -0500

    add Slack ID

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,30 @@ class User < ApplicationRecord
 
   def self.find_or_create_from_omniauth(auth)
     hca_id = auth.uid
+    slack_id = auth.extra.raw_info.slack_id
     raise "Missing HCA user ID from authentication" if hca_id.blank?
 
-    find_or_create_by!(hca_id:) do |user|
-      user.email = auth.info.email
-      user.name = auth.info.name
-      user.hca_access_token = auth.credentials.token
+    user = find_by(hca_id:) || find_by(slack_id:)
+
+    if user
+      user.update(
+        hca_id:,
+        slack_id:,
+        email: auth.info.email,
+        name: auth.info.name,
+        hca_access_token: auth.credentials.token
+      )
+    else
+      user = create!(
+        hca_id:,
+        slack_id:,
+        email: auth.info.email,
+        name: auth.info.name,
+        hca_access_token: auth.credentials.token
+      )
     end
+
+    user
   end
 
   def hca_profile(access_token) = HCAService.new(access_token).me
