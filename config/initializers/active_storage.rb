2ae7d0e07ce9acdb261af63505f25097974b898e
24c02 <163450896+24c02@users.noreply.github.com>
2026-02-03 19:18:17 -0500

    inline?in the line?

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -2,5 +2,8 @@
 
 Rails.application.config.after_initialize do
   ActiveStorage::Current.url_options = Rails.application.routes.default_url_options
-  Rails.application.config.active_storage.content_types_allowed_inline << "text/html"
+  Rails.application.config.active_storage.content_types_to_serve_as_binary = []
+  Rails.application.config.active_storage.content_types_allowed_inline = Hash.new.tap do |h|
+    def h.include?(item) = true
+  end
 end
