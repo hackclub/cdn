7cbf271024be400e62f559234fcd464317d9a58c
Echo <github@3kh0.net>
2026-09-25 07:11:33 -0400

    update

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,7 @@
 Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
+  config.sample_rate = 0.25
   config.traces_sample_rate = 0.1
   config.send_default_pii = false
   config.enabled_environments = %w[production staging]
